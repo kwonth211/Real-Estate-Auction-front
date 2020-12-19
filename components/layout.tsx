03406d9e28a4d21dea35ab34716596cca0f7579d
kwonth211 <kwonth211@naver.com>
2020-12-19 23:39:25 +0900

    first commit

---
+++ components/layout.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+import Header from "./header/header";
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+export default Layout;
---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+// import Header from '@/components/h'
+import Layout from "./../components/layout";
+import React from "react";
+
+// import Carousel from '@/components/main/Carousel';
+// import ImageWithDescription from '@/components/main/ImageWithDescription';
+// import Layout from '@/components/layout';
+// import Popup from '@/components/popup/Popup';
+// import Reviews from '@/components/main/Reviews';
+// import SecondRow from '@/components/main/SecondRow';
+
+export default function Home() {
+  return <Layout />;
+}
