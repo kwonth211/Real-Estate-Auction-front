423ee1cb849e8ff365269703ac1c3c15e447c1c2
kwonth211 <kwonth211@naver.com>
2021-01-10 22:20:07 +0900

    change global style

--- components/page.tsx
+++ components/page.tsx
@@ -1,17 +1,47 @@
-//
-// Copyright 2020 Fastcampus Language
-//
 import React from "react";
 import { Layout } from "antd";
-import { ThemeProvider } from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 import { withRouter } from "next/router";
 
-// import MainMenu from './MainMenu';
-import { Container, Inner } from "./styles/Page";
-import { theme } from "./styles/GlobalStyles.js";
-// import { useAppState } from './shared/AppProvider';
-// import { useSession } from '@/hooks/session';
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
+  background-color: white;
+  &.boxed {
+    overflow: hidden;
+    margin-right: auto;
+    margin-left: auto;
+  }
+  @media (min-width: 992px) {
+    &.boxed {
+      max-width: 95%;
+    }
+  }
+  .workspace {
+    height: ${(props) => `calc(100vh - ${props.theme.layoutHeaderHeight})`};
+    width: 100%;
+    display: flex;
+    position: relative;
+    overflow: hidden;
+    max-width: 100%;
+    flex-grow: 1;
+    flex-direction: row;
+  }
+  .workspace > .ant-layout {
+    overflow-x: hidden;
+  }
+`;
 
+const Inner = styled.div`
+  margin: 0 auto;
+  padding: 1.5rem;
+  position: relative;
+  background-color: white;
+  min-height: ${(props) => `calc(100vh - ${props.theme.layoutHeaderHeight})`};
+`;
 const { Content } = Layout;
 
 const NonDashboardRoutes = [
@@ -25,20 +55,20 @@ const NonDashboardRoutes = [
 const Page = ({ router, children }) => {
   //   const [state] = useAppState();
   //   const sessionExist = useSession();
-  const isNotDashboard = NonDashboardRoutes.includes(router.pathname);
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container>
-        <Layout className="workspace">
-          <Layout>
-            <Content>
-              <Inner>{children}</Inner>
-            </Content>
-          </Layout>
-        </Layout>
-      </Container>
-    </ThemeProvider>
+    // <ThemeProvider>
+    // <Container>
+    <Layout>
+      <Layout>
+        <Content>
+          {children}
+          {/* <Inner>{children}</Inner> */}
+        </Content>
+      </Layout>
+    </Layout>
+    // </Container>
+    // </ThemeProvider>
   );
 };
 
